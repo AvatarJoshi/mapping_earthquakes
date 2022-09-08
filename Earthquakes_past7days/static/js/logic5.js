45cb1878fc236d5934ac86109f75fe80d336e6bb
Avatar Joshi <joshi.avatar@gmail.com>
2022-09-08 16:06:07 -0700

    Added figure legend

--- Earthquakes_past7days/static/js/logic5.js
+++ Earthquakes_past7days/static/js/logic5.js
@@ -141,3 +141,33 @@ L.geoJSON(data, {
     });
 
 
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "#ea822c",
+  "#ea2c2c"
+];
+// Looping through our intervals to generate a label with a colored square for each interval.
+for (var i = 0; i < magnitudes.length; i++) {
+  console.log(colors[i]);
+  div.innerHTML +=
+    "<i style='background: " + colors[i] + "'></i> " +
+    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+}
+return div;
+};
+
+legend.addTo(map);
+
+
